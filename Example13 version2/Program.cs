1db0552335a0a7e55fc47a1d56a02b93d4a4fd0b
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-16 19:55:30 +0300

    Решили задачу 13 вторыцм способом

---
+++ Example13 version2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number<100)
+{
+Console.WriteLine($" {Number} - > третьей цифры нет!");
+}
+else
+{
+Console.WriteLine($"{Number} - > {Number.ToString()[2]}");
+}
